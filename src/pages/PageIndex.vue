77f215cf8d1a550f91ee3a1f06792951ed64f8d4
Thiago Moreira <thiago.cmoreira@yahoo.com.br>
2019-01-30 15:36:24 -0200

    :lipstick: Coming soon minor changes

--- src/pages/PageIndex.vue
+++ src/pages/PageIndex.vue
@@ -1,12 +1,11 @@
 <template lang="pug">
   q-page.page-index.flex.flex-center.column
-    h1.page-index__title Thiago Moreira - Personal Website
+    h1.page-index__title Thiago Moreira
     p.page-index__text Coming Soon
     div.page-index__buttons.flex
-      q-btn(@click="redirectTo('github.com/thiagocmoreira')", icon="mdi-github-circle" color="grey-1" flat round)
-      q-btn(@click="redirectTo('gitlab.com/thiagocmoreira')", icon="mdi-gitlab" color="grey-1" flat round)
-      q-btn(@click="redirectTo('linkedin.com/in/thiagocmoreira')" ,icon="mdi-linkedin-box" color="grey-1" flat round)
-      q-btn(@click="redirectTo('instagram.com/thiagueeras')" icon="mdi-instagram" color="grey-1" flat round)
+      q-btn(@click="redirectTo('github.com/thiagocmoreira')", icon="mdi-github-circle" color="grey-2" flat round)
+      q-btn(@click="redirectTo('gitlab.com/thiagocmoreira')", icon="mdi-gitlab" color="orange-8" flat round)
+      q-btn(@click="redirectTo('linkedin.com/in/thiagocmoreira')" ,icon="mdi-linkedin-box" color="blue-6" flat round)
 </template>
 
 <script>
@@ -28,25 +27,23 @@ export default {
 
   &__title
     font-size 42px
-    margin 0 0 30px 0
+    margin 0 0 15px 0
     font-weight 500
     text-align center
     @media(max-width: 735px)
       font-size 36px
     @media(max-width: 630px)
       font-size 28px
-      margin 0 0 20px 0
     @media(max-width: 414px)
       padding 0 25px
       font-size 22px
-      margin 0 0 15px 0
 
   &__text
     text-align center
-    font-size 28px
+    font-size 24px
     margin 0 0 30px 0
     @media(max-width: 630px)
-      font-size 22px
+      font-size 20px
       margin 0 0 20px 0
     @media(max-width: 414px)
       margin 0 0 15px 0
@@ -55,7 +52,7 @@ export default {
   &__buttons
     .q-btn
       margin 0 5px
-      font-size 26px
+      font-size 22px
       padding 0
       @media(max-width: 630px)
         font-size 20px
